ba1d77c6e558a3b4ac3920c5df14c23de2311b0e
Kartik <hi@kartikt.com>
2012-02-26 21:31:53 -0800

    ucwords padding fix

--- String/ucwords.py
+++ String/ucwords.py
@@ -1,8 +1,10 @@
 def ucwords(string):
     single   = string.split(' ')
     response = ''
+    last = len(single)
     
     for i in range(len(single)):
-        response += single[i][0].upper() + single[i][1:] + ' '
+        padding   = '' if i == last-1 else ' '
+        response += single[i][0].upper() + single[i][1:] + padding
 
     return response
